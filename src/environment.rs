452bcf23fcf977893d632af823b377e95c56fa7a
kanarus <kanarus786@gmail.com>
2025-06-26 18:15:10 +0900

    TODO: action <-> control / 'agent' idea implecitly included in `Environment`

--- src/environment.rs
+++ src/environment.rs
@@ -6,8 +6,8 @@ pub trait Environment {
 
     fn reset(&mut self) -> TimeStep<Self::Observation>;
     fn step(&mut self, action: &Self::Action) -> TimeStep<Self::Observation>;
-    fn action_spec(&self) -> &dyn Space<Sample = Self::Action>;
-    fn observation_spec(&self) -> &dyn Space<Sample = Self::Observation>;
+    fn action_spec(&self) -> ArraySpec;
+    fn observation_spec(&self) -> ArraySpec;
 }
 
 pub trait Action {
@@ -27,6 +27,17 @@ pub enum StepType {
     Last,
 }
 
+pub struct ArraySpec {
+    /// Currently not used
+    pub dtype: DType,
+    pub shape: Shape,
+}
+/// Currently not used
+pub enum DType {
+    F64,
+    I32,
+    U8,
+}
 pub enum Shape {
     Scaler,
     Vector(usize),
@@ -34,58 +45,72 @@ pub enum Shape {
     Tensor(usize, usize, usize),
 }
 
-/// Currently the data type is fixed to `f64`.
-pub trait Space {
-    /// The type of the sample in this space.
-    type Sample;
-
-    /// The shape of the sample in this space.
-    fn shape(&self) -> Shape;
-}
+// /// Currently the data type is fixed to `f64`.
+// pub trait Space {
+//     /// The type of the sample in this space.
+//     type Sample;
+// 
+//     /// The shape of this space.
+//     fn shape(&self) -> Shape;
+// }
 
 /////////////////////////////////////////////////////////////////
 
-pub struct Composer(ComposedEnvironment);
+pub struct Composer<O = ()>(ComposedEnvironment<O>);
 
-pub struct ComposedEnvironment {
+pub struct ComposedEnvironment<O> {
     physics: Physics,
+    observer: Option<Box<dyn Observer<Observation = O>>>,
     task: Option<Box<dyn Task>>,
     rewarders: Vec<Box<dyn Rewarder>>,
-    observables: Vec<Box<dyn Observable>>,
 }
 
-/// ## Default
+pub trait Observer {
+    type Observation;
+    fn observe(&self, physics: &Physics) -> Self::Observation;
+}
+
+/// A task defines the goal of the environment and how to determine if the task is complete.
 /// 
-/// - `initialize_episode`: does nothing
-/// - `should_terminate`: always returns `false`, never terminates the episode
+/// Rewards are speparated from tasks to allow for more flexible reward structures.
+/// See [`Rewarder`] and [`Composer`].
+/// 
+/// ### required
+/// 
+/// - `should_terminate(&self, Physics) -> bool`: Determines if the task is complete
 pub trait Task {
-    fn initialize_episode(&self, physics: &mut Physics) {
+    fn initialize_episode(&self, #[allow(unused)] physics: &mut Physics) {
         // do nothing by default
     }
 
-    fn should_terminate(&self, physics: &Physics) -> bool {
-        false
-    }
+    fn should_terminate(&self, physics: &Physics) -> bool;
 }
 
 pub trait Rewarder {
     fn give(&self, physics: &Physics) -> f64;
 }
 
-pub trait Observable {
-    fn observe(&self, physics: &Physics) -> Vec<f64>;
-}
-
 impl Composer {
     pub fn new(physics: Physics) -> Self {
         Self(ComposedEnvironment {
             physics,
+            observer: None,
             task: None,
             rewarders: Vec::new(),
-            observables: Vec::new(),
         })
     }
 
+    pub fn observer<O>(self, observer: impl Observer<Observation = O> + 'static) -> Composer<O> {
+        Composer(ComposedEnvironment {
+            observer: Some(Box::new(observer)),
+            physics: self.0.physics,
+            task: self.0.task,
+            rewarders: self.0.rewarders,
+        })
+    }
+}
+
+impl<O> Composer<O> {
     pub fn task(mut self, task: impl Task + 'static) -> Self {
         self.0.task = Some(Box::new(task));
         self
@@ -96,13 +121,15 @@ impl Composer {
         self
     }
 
-    pub fn add_observable(mut self, observable: impl Observable + 'static) -> Self {
-        self.0.observables.push(Box::new(observable));
-        self
+    /// Composes the final environment.
+    pub fn compose(self) -> impl Environment {
+        self.0
     }
 }
 
-impl Environment for ComposedEnvironment {
+impl<O, A: Action<Environment = ComposedEnvironment<O>>> Environment for ComposedEnvironment<O> {
+    type Observation = O;
+    type Action = A;
 
 }
 
