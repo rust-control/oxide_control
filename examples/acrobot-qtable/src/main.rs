77522b7519d512cbbb0dacebaae88a27de7a2d7d
kanarus <kanarus786@gmail.com>
2025-07-10 06:27:51 +0900

    TODO: acrobot

--- examples/acrobot-qtable/src/main.rs
+++ examples/acrobot-qtable/src/main.rs
@@ -1,21 +1,24 @@
 use qtable::{QTable, QUpdate, strategy};
-use oxide_control::physics::binding::obj;// TODO: reexport at more shallow place
+use oxide_control::physics::{ObjectId, obj, joint};
 
-struct Acrobot(oxide_control::RawPhysics);
+struct Acrobot {
+    raw: oxide_control::RawPhysics,
+    elbow: ObjectId<obj::Joint>,
+    shoulder: ObjectId<obj::Joint>,
+}
 
 impl std::ops::Deref for Acrobot {
     type Target = oxide_control::RawPhysics;
 
     fn deref(&self) -> &Self::Target {
-        &self.0
+        &self.raw
     }
 }
 impl std::ops::DerefMut for Acrobot {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
+        &mut self.raw
     }
 }
-
 impl oxide_control::Physics for Acrobot {}
 
 struct AcrobotObservation {
@@ -27,25 +30,23 @@ struct Orientation {
     sin: f64,
     cos: f64,
 }
+impl Orientation {
+    fn from_rad(rad: f64) -> Self {
+        Self { sin: rad.sin(), cos: rad.cos() }
+    }
+}
 
 impl oxide_control::Observation for AcrobotObservation {
     type Physics = Acrobot;
 
-// TODO: cache name->id lookups in Physics?
     fn generate(physics: &Self::Physics) -> Self {
-        let [elbow_orientation, shouder_orientation] = ["elbow", "shoulder"]
-            .map(|name| {
-                let id = physics.model().object_id_of::<obj::Joint>(name).unwrap();
-                // TODO: https://github.com/rust-control/rusty_mujoco/issues/2
-                let qpos_offset = physics.model().jnt_qposadr()[id.index()] as usize;
-                // TODO: prepare a better way to access and mutate `qpos` and others like `qvel`
-                let qpos = unsafe {
-                    let nq = physics.model().nq();
-                    physics.data().qpos(nq)
-                };
-                let rad = qpos[qpos_offset];
-                Orientation { sin: rad.sin(), cos: rad.cos() }
-            });
+        let elbow_rad = physics.qpos::<joint::Hinge>(physics.elbow).unwrap();
+        let shoulder_rad = physics.qpos::<joint::Hinge>(physics.shoulder).unwrap();
+        Self {
+            elbow_orientation: Orientation::from_rad(elbow_rad),
+            shoulder_orientation: Orientation::from_rad(shoulder_rad),
+            velocity: (), // TODO: fill in with actual velocity data
+        }
     }
 }
 
@@ -67,24 +68,9 @@ impl oxide_control::Task for BalanceTask {
         use std::f64::consts::PI;
         use rand::Rng;
 
-        let elbow_id = physics.model().object_id_of::<obj::Joint>("elbow").unwrap();
-        let shoulder_id = physics.model().object_id_of::<obj::Joint>("shoulder").unwrap();
-
-        // TODO: https://github.com/rust-control/rusty_mujoco/issues/2
-        let elbow_qpos_offset = physics.model().jnt_qposadr()[elbow_id.index()] as usize;
-        let shoulder_qpos_offset = physics.model().jnt_qposadr()[shoulder_id.index()] as usize;
-
-        let mut rng = rand::rng();
-        let qpos_mut = unsafe {// TODO: prepare a better way to access and mutate `qpos` and others like `qvel`
-            let nq = physics.model().nq();
-            physics.data_mut().qpos_mut(nq)
-        };
-        
-        // TODO: check each joint's qpos element size
-        // (here we just already know that `elbow` and `shoulder` are both 1 element)
         let elbow_qpos_range = if self.do_swing {-(PI/2.)..(PI/2.)} else {-(PI/10.)..(PI/10.)};
-        qpos_mut[elbow_qpos_offset] = rng.random_range(elbow_qpos_range);
-        qpos_mut[shoulder_qpos_offset] = 0.;
+        physics.set_qpos::<joint::Hinge>(physics.elbow, [rng.random_range(elbow_qpos_range)]);
+        physics.set_qpos::<joint::Hinge>(physics.shoulder, [0.]);
     }
 
     fn should_finish_episode(&self, observation: &Self::Observation, physics: &Self::Physics) -> bool {
